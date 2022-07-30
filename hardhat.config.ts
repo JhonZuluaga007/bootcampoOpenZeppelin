28c7080757339968480c61096584b0ad31146ff1
jhonzusa <jzuluaga@pastpost.com>
2022-07-29 20:15:29 -0500

    the respective verification is added for the deployment of the blockchain, configuration is also added to be able to verify the contract and see how much gas you spend

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,57 @@
-import { HardhatUserConfig } from "hardhat/config";
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-ethers";
+import "hardhat-gas-reporter";
+
+
+import { accounts } from "./utils/network";
+
+dotenv.config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      accounts: accounts()
+    },
+    ropsten: {
+      url: process.env.ROPSTEN_URL || "",
+      accounts: accounts("ropsten"),
+    },
+    mumbai: {
+      url: process.env.MUMBAI_URL || "",
+      accounts: accounts("mumbai"),
+    },
+    goerli: {
+      url: process.env.GOERLI_URL || "",
+			accounts: accounts("goerli"),
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
